aa5a606a30ad3924ab046e03ead280e2baa421b6
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2022-12-31 00:40:10 -0600

    Changed portfolio

--- js/main.js
+++ js/main.js
@@ -113,26 +113,26 @@ scrollTopBtn.addEventListener("click", () => {
 });
 
 //Navigation menu items active on page scroll
-window.addEventListener("scroll", () => {
-    const sections = document.querySelectorAll("section");
-    const scrollY = window.pageYOffset;
+// window.addEventListener("scroll", () => {
+//     const sections = document.querySelectorAll("section");
+//     const scrollY = window.pageYOffset;
 
-    sections.forEach((current) => {
-        let sectionHeight = current.offsetHeight;
-        let sectionTop = current.offsetTop - 50;
-        let id = current.getAttribute("id");
+//     sections.forEach((current) => {
+//         let sectionHeight = current.offsetHeight;
+//         let sectionTop = current.offsetTop - 50;
+//         let id = current.getAttribute("id");
 
-        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-            document
-                .querySelector(".nav-items a[href*=" + id + "]")
-                .classList.add("active");
-        } else {
-            document
-                .querySelector(".nav-items a[href*=" + id + "]")
-                .classList.remove("active");
-        }
-    });
-});
+//         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+//             document
+//                 .querySelector(".nav-items a[href*=" + id + "]")
+//                 .classList.add("active");
+//         } else {
+//             document
+//                 .querySelector(".nav-items a[href*=" + id + "]")
+//                 .classList.remove("active");
+//         }
+//     });
+// });
 
 //Responsive navigation menu toggle
 const menuBtn = document.querySelector(".nav-menu-btn");
