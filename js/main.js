1dd8ab6796d230aed3c70da67e7384221b5dd8d4
Sy Tu Nguyen <54698578+sytunguyen77@users.noreply.github.com>
2023-04-25 00:45:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -178,39 +178,39 @@ ScrollReveal().reveal(".home .info h2, .section-title-01, .section-title-02", {
    origin: "left",
 });
 ScrollReveal().reveal(".home .info h3, .home .info p, .about-info .btn", {
-   delay: 600,
+   delay: 100,
    origin: "right",
 });
 ScrollReveal().reveal(".home .info .btn", { delay: 700, origin: "bottom" });
 ScrollReveal().reveal(".media-icons i, .contact-left li", {
-   delay: 500,
+   delay: 100,
    origin: "left",
    interval: 200,
 });
 ScrollReveal().reveal(".home-img, .about-img", {
-   delay: 500,
+   delay: 100,
    origin: "bottom",
 });
 ScrollReveal().reveal(".about .description, .contact-right", {
-   delay: 600,
+   delay: 100,
    origin: "right",
 });
 ScrollReveal().reveal(".about .professional-list li", {
-   delay: 500,
+   delay: 100,
    origin: "right",
    interval: 200,
 });
 ScrollReveal().reveal(
    ".skills-description, .services-description, .contact-card, .client-swiper, .contact-left h2, .skills-box, .skills-toggle",
-   { delay: 700, origin: "left" }
+   { delay: 100, origin: "left" }
 );
 ScrollReveal().reveal(".experience-card, .service-card, .education, .portfolio .portfolio-item", {
-   delay: 800,
+   delay: 100,
    origin: "bottom",
    interval: 200,
 });
 ScrollReveal().reveal("footer .group", {
-   delay: 500,
+   delay: 100,
    origin: "top",
    interval: 200,
 });
